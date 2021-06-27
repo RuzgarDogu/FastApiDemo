c95d935666a87d0f8ee1bd46a4adb90c0da249bd
RuzgarDogu <doguruzgar@gmail.com>
2021-06-27 23:39:38 +0300

    v04

--- api/utils/database.py
+++ api/utils/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import mysql.connector
 
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}/fastapi".format('root', '', 'localhost')
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}/fastapi".format('root', '', 'localhost')
+# engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
